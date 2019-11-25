143f2b11b925ba4a028557b2f463142be15c3996
waspryo <wasp2123321@gmail.com>
2019-11-25 23:47:55 +0900

    SEO

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1>About our Nuxt's</h1>
+    <p class="subheading">They're juicy and stuff.</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed doloribus rem, </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+    head() {
+        return {
+            title: 'About Nuxt',
+            meta: [
+                {name: 'description', content: 'thix is my about description here.'},
+                {name: 'keywords', content: 'about nuxt, nuxt info'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
