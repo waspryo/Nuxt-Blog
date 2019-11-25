ac958bc697f82ac611d178d0b35a03d01b68c81d
waspryo <wasp2123321@gmail.com>
2019-11-25 23:29:27 +0900

    create nav-bar

--- layouts/partials/nav.vue
+++ layouts/partials/nav.vue
@@ -16,4 +16,32 @@
 </script>
 
 <style>
+header {
+        background: rgb(0, 219, 146);
+        display: grid;
+        grid-template-columns: repeat(2,auto);
+    }
+    .logo, li a {
+        padding: 1em 2em;
+        display: block;
+        text-transform: uppercase;
+        text-decoration: none;
+        font-weight: bold;
+        color: white;
+        font-size: .9em;
+    }
+    nav {
+        justify-self: right;
+    }
+    ul {
+        list-style-type: none;
+    }
+    li {
+        display: inline;
+    }
+    li a {
+        padding: 1em 2em;
+        display: inline-block;
+        background: rgba(0,0,0,0.1);
+    }
 </style>
