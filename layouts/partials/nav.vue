37723be2ccb3ebeba12740a42a116c9abd0263ae
waspryo <wasp2123321@gmail.com>
2019-11-25 23:22:49 +0900

    create nuxt-link

---
+++ layouts/partials/nav.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <nuxt-link to="/" class="logo">Nuxty</nuxt-link>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="about">About</nuxt-link></li>
+        <li><nuxt-link to="services">Services</nuxt-link></li>
+        <li><nuxt-link to="contact">Contact</nuxt-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
